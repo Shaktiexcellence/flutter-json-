04d3b24690d9b39c35b1e56aaf3d70998290d9e1
Shaktiexcellence <shakti@excellencetechnologies.in>
2020-12-01 13:07:44 +0530

    josn study started

---
+++ lib/home_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_json/model/data.dart';
+import 'package:http/http.dart' as Http;
+import 'dart:convert';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<Data>> getAllData() async {
+    var api = "https://jsonplaceholder.typicode.com/photos";
+    var data = await Http.get(api);
+    var jsonData = json.decode(data.body);
+    List<Data> listOf = [];
+    for (var i in jsonData) {
+      Data data = new Data(i["id"], i["url"], i["thumbnail"], i["title"]);
+      listOf.add(data);
+    }
+    return listOf;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("json app"),
+        backgroundColor: Colors.deepOrange,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search), onPressed: () => print("search")),
+          IconButton(icon: Icon(Icons.add), onPressed: () => print("add"))
+        ],
+      ),
+      drawer: Drawer(
+          child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text("DrawerHeader"),
+            decoration: BoxDecoration(color: Colors.deepOrange),
+          ),
+          ListTile(
+            leading: Icon(Icons.search, color: Colors.orange),
+            title: Text("First Page"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.add, color: Colors.red),
+            title: Text("Second Page"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.title, color: Colors.purple),
+            title: Text("Third Page"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.list, color: Colors.green),
+            title: Text("Forth Page"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          Divider(
+            height: 5,
+          ),
+          ListTile(
+            leading: Icon(Icons.close, color: Colors.red),
+            title: Text("Close Page"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      )),
+      body: ListView(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(10),
+              height: 250,
+              child: FutureBuilder(
+                  future: getAllData(),
+                  builder: (BuildContext c, AsyncSnapshot snapshot) {
+                    if (snapshot.data == null) {
+                      return Center(
+                          child: Container(child: Text("Loading...")));
+                    } else {
+                      return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        itemBuilder: (BuildContext c, int index) {
+                          return Card(
+                              elevation: 10,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Image.network(snapshot.data[index].url,
+                                      height: 150,
+                                      width: 150,
+                                      fit: BoxFit.cover),
+                                  SizedBox(height: 7),
+                                  Container(
+                                      margin: EdgeInsets.all(6),
+                                      height: 50,
+                                      child: Row(
+                                        children: <Widget>[
+                                          Container(
+                                              child: CircleAvatar(
+                                            child: Text(snapshot.data[index].id
+                                                .toString()),
+                                          )),
+                                          SizedBox(width: 6),
+                                          Container(
+                                              width: 80,
+                                              child: Text(
+                                                snapshot.data[index].title,
+                                                maxLines: 1,
+                                                style: TextStyle(
+                                                    color: Colors.deepOrange),
+                                              )),
+                                        ],
+                                      ))
+                                ],
+                              ));
+                        },
+                      );
+                    }
+                  }))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/model/data.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+
+class Data {
+  int id;
+  String url, thumbnail, title;
+  Data(this.id, this.url, this.thumbnail, this.title);
+}
